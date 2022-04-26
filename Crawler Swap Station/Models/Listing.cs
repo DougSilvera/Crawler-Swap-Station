efcc83e397a457f436a824fdc4279a40f3c8c3df
DougSilvera <shortxjdoug@gmail.com>
2022-04-26 16:32:35 -0400

    listing scaffold and methods

---
+++ Crawler Swap Station/Models/Listing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Crawler_Swap_Station.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public int Price { get; set; }
+        public int UserId { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public int ScaleId { get; set; }
+        public int VehicleId { get; set; }
+
+    }
+}
