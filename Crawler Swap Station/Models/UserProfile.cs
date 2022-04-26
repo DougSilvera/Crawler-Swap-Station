b8bbd82e53db851b20e2914a91dcf67ec1310ad3
DougSilvera <shortxjdoug@gmail.com>
2022-04-26 14:39:02 -0400

    firebase implementation

---
+++ Crawler Swap Station/Models/UserProfile.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crawler_Swap_Station.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseUserId { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
